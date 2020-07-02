dc4a0b43ca7bc6b137c96b3b2b249b4b99dfa9a2
Eivind Holt <eivholt@gmail.com>
2020-07-02 23:19:24 +0200

    Create observation

--- Environment/Google Healthcare API/CreateObservation.ps1
+++ Environment/Google Healthcare API/CreateObservation.ps1
@@ -4,10 +4,10 @@ $PROJECT_ID = "iothealthcare-269209"
 $LOCATION = "europe-west4"
 $DATASET_ID = "IoTHealthcareDataset"
 $FHIR_STORE_ID = "ObservationsDataStore"
-#$PATIENT_ID = "9f6a2621-0f66-464f-bd9b-f6a54f2222de" #Ripley
+$PATIENT_ID = "9f6a2621-0f66-464f-bd9b-f6a54f2222de" #Ripley
 #$PATIENT_ID = "d0be3465-6956-46b0-96fb-9f844a093902" #Weyland
-$PATIENT_ID = "1c8fa469-2b2b-4da2-9205-51177c127244" #Hicks
-$ENCOUNTER_ID = "5accad37-c56d-469d-9629-23e9a552c9a1"
+#$PATIENT_ID = "1c8fa469-2b2b-4da2-9205-51177c127244" #Hicks
+$ENCOUNTER_ID = "5accad37-c56d-469d-9629-23e9a552c9a1" #Ripley encounter 1
 $DATETIMENOW = Get-Date -Format o
 $BPM_VALUE = Get-Random -Minimum 50 -Maximum 220
 
--- Service/GoogleHealthcareFunctionApp/ObservationFunctions.cs
+++ Service/GoogleHealthcareFunctionApp/ObservationFunctions.cs
@@ -27,52 +27,76 @@ namespace GoogleHealthcareFunctionApp
         private const string DatasetId = "IoTHealthcareDataset";
         private const string FhirStoreId = "ObservationsDataStore";
 
-        [FunctionName("PatchObservation")]
+        [FunctionName("CreateObservation")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext executionContext)
         {
-            log.LogInformation("C# HTTP trigger function PatchObservation processed a request.");
+            log.LogInformation("C# HTTP trigger function CreateObservation processed a request.");
 
             using (var cloudHealthcareService = CreateCloudHealthcareService(executionContext))
             {
                 try
                 {
                     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                    var requestJson = JsonConvert.DeserializeObject<JObject>(requestBody);
+                    var requestJson = JsonConvert.DeserializeObject<JObject>(requestBody) as dynamic;
 
                     var telemetryDictionary = new Dictionary<string, object>();
                     ExtractTelemetry(requestJson, telemetryDictionary);
                     LogTelemetry(log, telemetryDictionary);
 
-                    // Observation id to patch
-                    var resourceId = "58bfeb6f-f3bd-424c-a6f9-2815af956ae9";
-                    // The name of the resource to patch.
-                    string resourceName = $"projects/{ProjectId}/locations/{Location}/datasets/{DatasetId}/fhirStores/{FhirStoreId}/fhir/Observation/{resourceId}";
+                    // Patient id to add observation to
+                    var patientId = "9f6a2621-0f66-464f-bd9b-f6a54f2222de";
+                    // Encounter id to add observation to
+                    var encounterId = "5accad37-c56d-469d-9629-23e9a552c9a1";
+                    // The name of the resource to create.
+                    string parent = $"projects/{ProjectId}/locations/{Location}/datasets/{DatasetId}/fhirStores/{FhirStoreId}";
+                    var createBody = CreateObservationHttpBody(requestJson, telemetryDictionary, patientId, encounterId);
 
-                    Data.HttpBody patchBody = new Data.HttpBody();
+                    var createRequest =
+                        cloudHealthcareService.Projects.Locations.Datasets.FhirStores.Fhir.Create(createBody, parent, "Observation");
 
-                    var jsonPatch = new JArray();
-                    var patchValue = new JObject();
-                    patchValue.Add("op", "replace");
-                    patchValue.Add("path", "/valueQuantity/value");
-                    patchValue.Add("value", hrValue.Value);
-                    jsonPatch.Add(patchValue);
-
-                    patchBody.Data = jsonPatch.ToString();
-
-                    var patchRequest =
-                        cloudHealthcareService.Projects.Locations.Datasets.FhirStores.Fhir.Patch(patchBody, resourceName);
-
-                    JObject response = patchRequest.PatchAsJObjectAsync(patchBody).Result;
+                    JObject response = createRequest.SendAsJObjectAsync(createBody).Result;
 
                     return new OkObjectResult(response);
                 }
                 catch (Exception ex)
                 {
-                    return new BadRequestObjectResult(ex);
+                    return new BadRequestObjectResult(JsonConvert.SerializeObject(ex));
                 }
             }
+        }
+
+        private static HttpBody CreateObservationHttpBody(dynamic requestJson, Dictionary<string, object> telemetryDictionary, string patientId, string encounterId)
+        {
+            Data.HttpBody createBody = new Data.HttpBody();
+
+            var observationJson = new JObject();
+            dynamic observation = observationJson;
+            observation.resourceType = "Observation";
+            observation.status = "final";
+            dynamic observationSubject = new JObject();
+            observationSubject.reference = $"Patient/{patientId}";
+            observation.subject = observationSubject;
+            observation.effectiveDateTime = requestJson.timestamp;
+            dynamic observationCode = new JObject();
+            observationCode.coding = new JArray() as dynamic;
+            dynamic observationCoding = new JObject();
+            observationCoding.system = "http://loinc.org";
+            observationCoding.code = "8867-4";
+            observationCoding.display = "Heart rate";
+            observationCode.coding.Add(observationCoding);
+            observation.code = observationCode;
+            dynamic observationValueQuantity = new JObject();
+            observationValueQuantity.value = telemetryDictionary["hr"];
+            observationValueQuantity.unit = "bpm";
+            observation.valueQuantity = observationValueQuantity;
+            dynamic observationContext = new JObject();
+            observationContext.reference = $"Encounter/{encounterId}";
+            observation.context = observationContext;
+
+            createBody.Data = observationJson.ToString();
+            return createBody;
         }
 
         private static void ExtractTelemetry(JObject requestJson, Dictionary<string, object> telemetryDictionary)
@@ -114,12 +138,12 @@ namespace GoogleHealthcareFunctionApp
 
     public static class CloudHealthcareServiceExtensions
     {
-        public static async Task<JObject> PatchAsJObjectAsync(this ClientServiceRequest<HttpBody> request, HttpBody requestBody)
+        public static async Task<JObject> SendAsJObjectAsync(this ClientServiceRequest<HttpBody> request, HttpBody requestBody)
         {
             var httpRequestMessage = request.CreateRequest();
             httpRequestMessage.Content = new StringContent(requestBody.Data);
-            httpRequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json-patch+json");
-            var httpResponseMessage = await request.Service.HttpClient.PatchAsync(httpRequestMessage.RequestUri.AbsoluteUri, httpRequestMessage.Content);
+            httpRequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/fhir+json");
+            var httpResponseMessage = await request.Service.HttpClient.PostAsync(httpRequestMessage.RequestUri.AbsoluteUri, httpRequestMessage.Content);
             httpResponseMessage.EnsureSuccessStatusCode();
             var responseText = await httpResponseMessage.Content.ReadAsStringAsync();
             return JObject.Parse(responseText);
